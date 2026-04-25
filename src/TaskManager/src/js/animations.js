82a161826285b27adb20ddb9ce3a79f83e9a0015
andreselianor <113841548+andreselianor@users.noreply.github.com>
2026-04-25 10:29:18 +0200

    chore: stable version 1.0

---
+++ src/TaskManager/src/js/animations.js
@@ -0,0 +1,20 @@
+var navtoggle = document.getElementsByClassName("icon-rightChevron")[0];
+var navtogglebool = true;
+var navlegends = document.getElementsByClassName("navtexts");
+navtoggle.addEventListener('click', displayLegend);
+
+function displayLegend(){
+    if(navtogglebool){
+    navtoggle.style.transform= "rotate(90deg)";
+        Array.from(navlegends).forEach(element => {
+            element.style.display = "flex";
+        });
+    }
+    if(!navtogglebool){
+    navtoggle.style.transform= "rotate(0deg)";
+        Array.from(navlegends).forEach(element => {
+            element.style.display = "none";
+        });
+    }
+    navtogglebool = !navtogglebool;
+}
